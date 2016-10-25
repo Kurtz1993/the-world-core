55abc1f8b7ebcbae3cda4d4f9848685edc06ec18
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-24 20:43:43 -0500

    Wrap the script in an IIFE

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,13 @@
-var ele = document.getElementById('username');
-ele.innerText = "Luis Hernandez";
+(function () {
+  var ele = document.getElementById('username');
+  ele.innerText = "Luis Hernandez";
 
-var main = document.getElementById('main');
-main.onmouseenter = function () {
-  main.style.backgroundColor = "#888";
-};
+  var main = document.getElementById('main');
+  main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+  };
 
-main.onmouseleave = function () {
-  main.style.backgroundColor = "";
-};
+  main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+  };
+})();
