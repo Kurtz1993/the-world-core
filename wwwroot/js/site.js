dac58fbea33c0891d0afcd672c5533ab224fddb0
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-24 20:42:18 -0500

    Add basic event listeners

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,11 @@
 var ele = document.getElementById('username');
 ele.innerText = "Luis Hernandez";
+
+var main = document.getElementById('main');
+main.onmouseenter = function () {
+  main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+  main.style.backgroundColor = "";
+};
