73ce323b2c8ac018ce659ada912f4d037c83f6cf
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 23:16:00 -0600

    Use configuration builder for Ef

--- Models/WorldContext.cs
+++ Models/WorldContext.cs
@@ -10,12 +10,13 @@ namespace TheWorld.Models
 
         private IConfigurationRoot _config;
 
-        public WorldContext(IConfigurationRoot config)
+        public WorldContext(IConfigurationRoot config, DbContextOptions options) : base(options)
         {
             _config = config;
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
         }
