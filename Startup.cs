fa315a9d814a3bc34bf6e0dc329248433c66404d
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 19:34:32 -0600

    Pull configuration from config.json

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using TheWorld.Services;
 
 namespace TheWorld
@@ -9,14 +9,23 @@ namespace TheWorld
     public class Startup
     {
         private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
         public Startup(IHostingEnvironment env)
         {
             _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json", false, true);
+
+            _config = builder.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
             if (_env.IsDevelopment())
             {
                 services.AddScoped<IMailService, DebugMailService>();
@@ -30,7 +39,7 @@ namespace TheWorld
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
             if (_env.IsDevelopment())
             {
