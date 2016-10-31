29fcdd12551bbc591955c409d934d76cd61b9bd3
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 23:41:35 -0600

    Add basic logging to debug

--- Models/WorldRepository.cs
+++ Models/WorldRepository.cs
@@ -2,19 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace TheWorld.Models
 {
     public class WorldRepository : IWorldRepository
     {
         private WorldContext _context;
-        public WorldRepository(WorldContext context)
+        private ILogger<WorldRepository> _logger;
+
+        public WorldRepository(WorldContext context, ILogger<WorldRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public IEnumerable<Trip> GetAllTrips()
         {
+            _logger.LogInformation("Getting all trips from the database");
             return _context.Trips.ToList();
         }
 
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TheWorld.Models;
 using TheWorld.Services;
 
@@ -40,15 +41,21 @@ namespace TheWorld
             services.AddDbContext<WorldContext>();
             services.AddScoped<IWorldRepository, WorldRepository>();
             services.AddTransient<WorldContextSeedData>();
+            services.AddLogging();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, WorldContextSeedData seeder)
+        public void Configure(IApplicationBuilder app, WorldContextSeedData seeder, ILoggerFactory factory)
         {
             if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                factory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                factory.AddDebug(LogLevel.Error);
             }
 
             app.UseStaticFiles();
