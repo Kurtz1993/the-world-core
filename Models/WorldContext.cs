bc3177b788e10728498bd12140efb4212489e816
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 22:56:26 -0600

    Add Dbcontext

---
+++ Models/WorldContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TheWorld.Models
+{
+    public class WorldContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+
+        public WorldContext()
+        {
+        }
+    }
+}
