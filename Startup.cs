7ec73284f9efe764fff7ab88da4fabca19b9bdda
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 19:28:27 -0600

    Add hosting environment as a private variable

--- Startup.cs
+++ Startup.cs
@@ -8,18 +8,31 @@ namespace TheWorld
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IMailService, DebugMailService>();
+            if (_env.IsDevelopment())
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                // Implement a real service.
+            }
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
