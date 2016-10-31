6fb4d0b0945d62cc747458676d9a6c5c4b4438ca
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 19:21:24 -0600

    Use MailService

--- Controllers/Web/AppController.cs
+++ Controllers/Web/AppController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,6 +19,13 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel vm)
+        {
+            _mailService.SendMail("me@me.com", vm.Email, "From The World", vm.Message);
             return View();
         }
 
