e0802f3d5738463c77ee25b7122177eafabf9d10
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-10-30 21:58:43 -0600

    Refactor toggle menu button

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,16 @@
 (function () {
   var $sidebarAndWrapper = $("#sidebar,#wrapper");
+  var $icon = $("#sidebar-toggle i.fa");
 
   $("#sidebar-toggle").on("click", function () {
     $sidebarAndWrapper.toggleClass("hide-sidebar");
 
     if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-      $(this).text("Show Sidebar");
+      $icon.removeClass("fa-chevron-left");
+      $icon.addClass("fa-chevron-right");
     } else {
-      $(this).text("Hide Sidebar");
+      $icon.removeClass("fa-chevron-right");
+      $icon.addClass("fa-chevron-left");
     }
   });
 
