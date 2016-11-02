1b5fb292355b48e76f5b4a85f212e0ff4411ee95
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-01 20:10:54 -0600

    Add POST method to create trips

--- Controllers/Api/TripsController.cs
+++ Controllers/Api/TripsController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Api
 {
+    [Route("api/trips")]
     public class TripsController : Controller
     {
         private IWorldRepository _repository;
@@ -12,10 +14,20 @@ namespace TheWorld.Controllers.Api
             _repository = repository;
         }
 
-        [HttpGet("api/trips")]
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok(_repository.GetAllTrips());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] TripViewModel trip)
+        {
+            if (ModelState.IsValid)
+            {
+                return Created($"api/trips/${trip.Name}", trip);
+            }
+            return BadRequest(ModelState);
         }
     }
 }
