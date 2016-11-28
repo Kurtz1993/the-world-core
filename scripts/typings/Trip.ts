bfac1566378b7486e409b60e0f8b96b4d7e4c9c6
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-27 22:10:21 -0600

    Include all typings in scripts folder

---
+++ scripts/typings/Trip.ts
@@ -0,0 +1,7 @@
+/** Represents a trip object. */
+declare interface Trip {
+    /** Name of the trip. */
+    name?: string;
+    /** Date the object was created. */
+    created?: string | Date;
+}
