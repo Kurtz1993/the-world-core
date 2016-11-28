718beb3fa4c6ae72802412b3a6ff6fd1329d7a8c
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-27 22:09:03 -0600

    Add hard-coded trips

--- scripts/trips.controller.ts
+++ scripts/trips.controller.ts
@@ -2,7 +2,18 @@ import { Inject } from "./decorators/decorators";
 
 @Inject()
 export class TripsController {
-    constructor() { }
+    public name: string = "Trips";
+    public trips: Trip[];
+
+    constructor() {
+        this.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            created: new Date()
+        }];
+    }
 
     /** Initializes the controller. */
     public $onInit(): void {}
