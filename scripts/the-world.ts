e4acf62ca8e302ea19e79512b86a44b06d9c4e58
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-27 22:35:46 -0600

    Register services as part of the application

--- scripts/the-world.ts
+++ scripts/the-world.ts
@@ -1,10 +1,14 @@
 import { TheWorldConfig } from "./the-world.config";
 import { TheWorldRun } from "./the-world.run";
+import { services } from "./services/services";
 import { TripsController } from "./trips.controller";
 
-angular.module("theWorld", [
-    "ui.router"
-])
-.controller("TripsController", TripsController)
-.config(TheWorldConfig)
-.run(TheWorldRun);
+angular
+    .module("theWorld", [
+        "ui.router",
+        services
+    ])
+    .controller("TripsController", TripsController)
+    .constant("env", { api: "/api" })
+    .config(TheWorldConfig)
+    .run(TheWorldRun);
