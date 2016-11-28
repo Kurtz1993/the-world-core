5aa9cfb76d4a951d9b9cceaf6afa70595c50188f
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-27 20:55:51 -0600

    Add Login view

---
+++ Controllers/Web/AuthController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+    }
+}
